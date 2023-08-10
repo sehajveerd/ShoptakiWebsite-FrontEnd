b146e17b4e78d20aade0b8b54275977e1741f8b5
ojasskapre <ojasskapre@gmail.com>
2023-08-10 16:03:09 +0530

    installed echarts library added the investment value over time chart

--- src/components/portfolio/InvestmentValueChart.jsx
+++ src/components/portfolio/InvestmentValueChart.jsx
@@ -1,14 +1,52 @@
-import React from 'react';
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useRef } from 'react';
+import * as echarts from 'echarts';
 
 // TODO: get required data as props for the chart
-const InvestmentValueChart = () => {
-  return (
-    <div>
-      <div className="absolute bottom-[43px] left-[24px] w-[707px] h-72">
-        Chart of Investment Value over period of time
-      </div>
-    </div>
-  );
+const InvestmentValueChart = ({ chartData }) => {
+  const chartRef = useRef(null);
+
+  useEffect(() => {
+    const myChart = echarts.init(chartRef.current);
+
+    const option = {
+      xAxis: {
+        type: 'category',
+        data: chartData.map(item => item.title), // TEMP: will need to update if the key is changed
+      },
+      yAxis: {
+        type: 'value',
+      },
+      series: [
+        {
+          data: chartData.map(item => item.value), // TEMP: will need to update if the key is changed,
+          type: 'line',
+          smooth: true,
+          lineStyle: {
+            color: '#0085ff',
+            shadowColor: '#3355de',
+            shadowBlur: 30,
+            shadowOffsetY: 20,
+          },
+        },
+      ],
+      tooltip: {
+        show: true,
+        valueFormatter: value => `$${value.toLocaleString()}`,
+        textStyle: {
+          fontFamily: 'Poppins',
+        },
+      },
+    };
+
+    myChart.setOption(option);
+
+    return () => {
+      myChart.dispose();
+    };
+  }, []);
+
+  return <div ref={chartRef} className="absolute w-full h-full"></div>;
 };
 
 export default InvestmentValueChart;
--- src/pages/portfolio.jsx
+++ src/pages/portfolio.jsx
@@ -5,6 +5,20 @@ import InvestmentInfoCard from '../components/portfolio/InvestmentInfoCard';
 import InvestmentValueChart from '../components/portfolio/InvestmentValueChart';
 
 const Portfolio = () => {
+  /**
+   * TODO: fetch the data required for all the cards using a single query and pass them to each card as props
+   */
+  // TEMP: this will be extracted from the fetched data. The keys of the objects in the list can be different. If different then update in the InvestmentValueChart component as well
+  const investmentValueChart = [
+    { title: 'May', value: '45523' },
+    { title: 'June', value: '38163' },
+    { title: 'July', value: '51672' },
+    { title: 'August', value: '59451' },
+    { title: 'September', value: '74123' },
+    { title: 'October', value: '64903' },
+    { title: 'November', value: '77651' },
+  ];
+
   return (
     <div className="relative bg-neutral-gray100f1f1f1 w-full min-h-screen overflow-y-auto text-left text-sm text-black font-body-xs12">
       <div className="absolute top-5 left-[272px] text-5xl font-semibold text-gray-900">
@@ -38,7 +52,7 @@ const Portfolio = () => {
       </div>
       <div className="absolute top-[276px] left-[272px] rounded-3xs bg-shades-white w-[754px] h-[362px] text-center text-xs text-neutral-gray500747474 font-inter">
         {/* TODO: pass required data as props to the chart */}
-        <InvestmentValueChart />
+        <InvestmentValueChart chartData={investmentValueChart} />
       </div>
       <div className="absolute top-[668px] left-[272px] text-xl rounded-xl bg-shades-white w-[1030px] h-12 overflow-hidden">
         <div className="absolute top-[9px] left-[21px] font-semibold">
