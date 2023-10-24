2bfef3eabfdae60a868f4dff3ac5a8b3c79d2d22
SwethaVoora <swetha.voora01@gmail.com>
2023-10-24 12:07:10 -0400

    Adding Label Component

---
+++ src/components/Label.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Label = props => {
+  const { text, required, htmlFor } = props;
+  return (
+    <>
+      <label htmlFor={htmlFor}>
+        <span className="font-bold">{text}</span>
+        {required && <span className="text-red-700">*</span>}
+      </label>
+    </>
+  );
+};
+
+export default Label;
